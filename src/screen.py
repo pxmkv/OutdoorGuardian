c61263ca5452ec552b66aadeaadfc1572d19052d
pxmkv <jitter_xie@foxmail.com>
2023-10-31 10:16:18 -0700

    screen test

--- src/screen.py
+++ src/screen.py
@@ -1,8 +1,22 @@
 from machine import Pin, I2C
 import ssd1306
+import time
 
 # using default address 0x3C
 i2c = I2C(sda=Pin(21), scl=Pin(22))
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
-display.text('Hello, World!', 0, 0, 1)
-display.show()
+
+while True:
+    display.text('Hello, World!', 0, 0, 1)
+    display.show()
+    time.sleep(1)
+    display.fill(0)
+
+    display.invert(1)
+
+    display.text('Bruh', 0, 0, 1)
+    display.show()
+    time.sleep(1)
+    display.fill(0)
+    display.invert(0)
+
