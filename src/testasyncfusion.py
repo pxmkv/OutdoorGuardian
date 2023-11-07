4c241b791b5a9ed99f99fed0ddfc3b0aef338179
ktay369 <ktay369@gmail.com>
2023-11-06 20:56:53 -0800

    Update testasyncfusion.py

--- src/testasyncfusion.py
+++ src/testasyncfusion.py
@@ -4,6 +4,8 @@ from imu import MPU6050
 from fusion_async import Fusion
 import ssd1306
 
+print("hello world")
+
 # Pins according the schematic https://heltec.org/project/wifi-kit-32/
 i2c_imu = I2C(-1, scl=Pin(25), sda=Pin(4))
 i2c = I2C(sda=Pin(21), scl=Pin(22))
@@ -11,6 +13,7 @@ display = ssd1306.SSD1306_I2C(128, 64, i2c)
 imu = MPU6050(i2c_imu)
 
 async def read_coro():
+    print("read coro")
     # Assuming the imu object has methods for non-blocking retrieval of data
     imu.mag_trigger()  # Hardware dependent: trigger a nonblocking read
     await asyncio.sleep_ms(20)  # Wait for mag to be ready
@@ -19,6 +22,7 @@ async def read_coro():
 fuse = Fusion(read_coro)
 
 async def display_orientation():
+    print("display orientation")
     await fuse.start()  # Start the asynchronous update task
     while True:
         display.fill(0)  # Clear the display
@@ -31,6 +35,7 @@ async def display_orientation():
 
 # Run the asynchronous loop
 async def main():
+    print("main")
     asyncio.create_task(display_orientation())
     # If you have other tasks to run, you can create them here
 
