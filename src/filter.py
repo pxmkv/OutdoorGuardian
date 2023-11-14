403e215ed58ddbcc563cc60b584f088bbbe94549
ktay369 <ktay369@gmail.com>
2023-11-13 21:29:30 -0800

    filter.py done

--- src/filter.py
+++ src/filter.py
@@ -2,6 +2,7 @@ from machine import Pin, I2C
 import utime
 import math
 from imu import MPU6050
+import ssd1306
  
 def calculate_tilt_angles(accel_data):
     x, y, z = accel_data[0], accel_data[1], accel_data[2]
@@ -24,6 +25,9 @@ def complementary_filter(pitch, roll, gyro_data, dt, alpha=0.98):
 i2c = I2C(0, scl=Pin(25), sda=Pin(4), freq=400000)
 mpu = MPU6050(i2c)
 
+i2c_screen = I2C(sda=Pin(21), scl=Pin(22))
+display = ssd1306.SSD1306_I2C(128, 64, i2c_screen)
+
 pitch = 0
 roll = 0
 prev_time = utime.ticks_ms()
@@ -43,5 +47,13 @@ while True:
     prev_time = curr_time
     
     print(tilt_x, tilt_y, tilt_z)
+
+    display.text('t '+str((int(tilt_x), int(tilt_y), int(tilt_z))), 0, 0, 1)
+    display.text('pr '+str((int(pitch), int(roll))), 0, 10, 1)
+    display.text('a '+str((int(accel_data[0]), int(accel_data[1]), int(accel_data[2]))), 0, 20, 1)
+    display.text('g '+str((int(gyro_data[0]), int(gyro_data[1]), int(gyro_data[2]))), 0, 30, 1)
+
     
-    utime.sleep(1)
+    display.show()
+    utime.sleep(0.5)
+    display.fill(0)
