8d9f458cbc2d9aaec79e8a78018847a5bcebc150
ktay369 <ktay369@gmail.com>
2023-11-14 10:52:27 -0800

    yaw
    added yaw

--- src/filter.py
+++ src/filter.py
@@ -13,14 +13,16 @@ def calculate_tilt_angles(accel_data):
  
     return tilt_x, tilt_y, tilt_z
  
-def complementary_filter(pitch, roll, gyro_data, dt, alpha=0.98):
+def complementary_filter(pitch, roll, yaw, gyro_data, dt, alpha=0.98):
     pitch += gyro_data[0] * dt
     roll -= gyro_data[1] * dt
+    yaw += gyro_data[2] * dt
  
     pitch = alpha * pitch + (1 - alpha) * math.atan2(gyro_data[1], math.sqrt(gyro_data[0] * gyro_data[0] + gyro_data[2] * gyro_data[2])) * 180 / math.pi
     roll = alpha * roll + (1 - alpha) * math.atan2(-gyro_data[0], math.sqrt(gyro_data[1] * gyro_data[1] + gyro_data[2] * gyro_data[2])) * 180 / math.pi
+    yaw = alpha * yaw + (1 - alpha) * math.atan2(math.sqrt(gyro_data[1] * gyro_data[1] + gyro_data[0] * gyro_data[0]), gyro_data[2]) * 180 / math.pi
  
-    return pitch, roll
+    return pitch, roll, yaw
 
 i2c = I2C(0, scl=Pin(25), sda=Pin(4), freq=400000)
 mpu = MPU6050(i2c)
@@ -30,6 +32,7 @@ display = ssd1306.SSD1306_I2C(128, 64, i2c_screen)
 
 pitch = 0
 roll = 0
+yaw = 0
 prev_time = utime.ticks_ms()
 
 while True:
@@ -42,18 +45,20 @@ while True:
     dt = (curr_time - prev_time) / 1000
 
     tilt_x, tilt_y, tilt_z = calculate_tilt_angles(accel_data)
-    pitch, roll = complementary_filter(pitch, roll, gyro_data, dt)
+    pitch, roll, yaw = complementary_filter(pitch, roll, yaw, gyro_data, dt)
 
     prev_time = curr_time
     
     print(tilt_x, tilt_y, tilt_z)
 
     display.text('t '+str((int(tilt_x), int(tilt_y), int(tilt_z))), 0, 0, 1)
-    display.text('pr '+str((int(pitch), int(roll))), 0, 10, 1)
-    display.text('a '+str((int(accel_data[0]), int(accel_data[1]), int(accel_data[2]))), 0, 20, 1)
-    display.text('g '+str((int(gyro_data[0]), int(gyro_data[1]), int(gyro_data[2]))), 0, 30, 1)
+    display.text('p '+str(int(pitch)), 0, 10, 1)
+    display.text('r '+str(int(roll)), 0, 20, 1)
+    display.text('y '+str(int(yaw)), 0, 30, 1)
+    display.text('a '+str((int(accel_data[0]), int(accel_data[1]), int(accel_data[2]))), 0, 40, 1)
+    display.text('g '+str((int(gyro_data[0]), int(gyro_data[1]), int(gyro_data[2]))), 0, 50, 1)
 
     
     display.show()
-    utime.sleep(0.5)
+    utime.sleep(0.1)
     display.fill(0)
