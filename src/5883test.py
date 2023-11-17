c593330e73ed002c7a2ecef882a59424e353ca51
Katie <katie@pop-os.localdomain>
2023-11-16 17:21:32 -0800

    modified compass test to return direction instead of x, y, z

--- src/5883test.py
+++ src/5883test.py
@@ -1,9 +1,27 @@
 from QMC5883 import QMC5883L
 from machine import Pin
 import time 
-qmc=QMC5883L(scl=22, sda=23)
+import math
+qmc=QMC5883L(scl=25, sda=4)
+
+def calculate_heading(x, y, declination=0):
+    heading = math.atan2(y, x)
+    heading_degrees = math.degrees(heading)
+    heading_degrees += declination
+
+    # Normalize to 0-360
+    if heading_degrees < 0:
+        heading_degrees += 360
+    elif heading_degrees > 360:
+        heading_degrees -= 360
+
+    return heading_degrees
+
 
 while True:
-    x, y, z, status, temp =qmc.read()
-    print (x, y, z, status, temp)
+    x, y, _, _, _ =qmc.read()
+    declination = -14  # Example declination value
+    heading = calculate_heading(x, y, declination)
+    print("Heading:", heading, "degrees")
+
     time.sleep(0.5)
