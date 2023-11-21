e68f039c2b29f80c1d7caaca5f21f372c96a6a1e
ktay369 <ktay369@gmail.com>
2023-11-21 11:50:47 -0800

    Add files via upload

--- src/magnet_to_dir.py
+++ src/magnet_to_dir.py
@@ -1,4 +1,13 @@
+from machine import I2C, Pin 
+from QMC5883 import QMC5883L
+import ssd1306
+import time
 import math
+
+i2c = I2C(sda=Pin(21), scl=Pin(22))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+compass = QMC5883L(scl=25, sda=4)
+
 
 def calculate_heading(x, y, declination=0):
     heading = math.atan2(y, x)
@@ -11,10 +20,15 @@ def calculate_heading(x, y, declination=0):
     elif heading_degrees > 360:
         heading_degrees -= 360
 
-    return heading_degrees
+    return heading_degrees 
+
+for i in range(500):
+    x, y, _, _, _ = compass.read()  # Assuming sensor is an instance of your magnetometer class
+    declination = 10  # Example declination value
+    heading = calculate_heading(x, y, declination)
+    print("Heading:", heading, "degrees")
+    time.sleep(0.1)
+
 
 # Example usage
-x, y, _, _, _ = sensor.read()  # Assuming sensor is an instance of your magnetometer class
-declination = 10  # Example declination value
-heading = calculate_heading(x, y, declination)
-print("Heading:", heading, "degrees")
+
