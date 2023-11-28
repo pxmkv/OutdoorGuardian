28e84c13fa0b0a4e0a28c512943575c24367477e
Joseph Vo <shmeepajarma@pop-os.localdomain>
2023-11-28 13:40:31 -0800

    another change

--- src/lora/lorawansend.py
+++ src/lora/lorawansend.py
@@ -15,7 +15,7 @@ counter = 0
 print("LoRa Sender")
 i2c = I2C(sda=Pin(21), scl=Pin(22))
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
-
+lora.begin(915E6)
 while True:
     payload = 'send help please'.format(counter)
     display.fill(0)  # Clear the display
