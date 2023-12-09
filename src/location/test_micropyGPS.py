45922de887661e99183fc55ff02d765060add0f5
Katie <katie@pop-os.localdomain>
2023-12-09 14:39:42 -0800

    encode/decode

--- src/location/test_micropyGPS.py
+++ src/location/test_micropyGPS.py
@@ -1,12 +1,40 @@
 from machine import UART, Pin
 import micropyGPS
 import time
+import json
+
+
+
 
 # Initialize GPS
-uart = UART(1, baudrate=9600, tx=14, rx=34)  # Update pins according to your hardware setup
+uart = UART(1, baudrate=9600, tx=17, rx=16)  # Update pins according to your hardware setup
 my_gps = micropyGPS.MicropyGPS()
 
-while True:
+
+def str_to_array(string):
+    if string != "":
+        return eval(string)
+    else:
+        return string
+
+    
+def convert_to_decimal(loc):
+    decimal = loc[0] + loc[1] / 60
+    if loc[2] in ['S', 'W']:
+        decimal = -decimal
+    return decimal
+
+
+
+
+def print_size_in_kb(data):
+    serialized_data = json.dumps(data)
+    size_in_bytes = len(serialized_data)
+    size_in_kb = size_in_bytes / 1024
+    print(f"Size: {size_in_kb:.2f} KB")
+
+
+def get_packet():
     if uart.any():
         my_sentence = uart.readline().decode('utf-8')
         for x in my_sentence:
@@ -15,15 +43,17 @@ while True:
 
         # Check if the data is valid
         if my_gps.valid:
-            print("valid! yay!!")
-            print('Latitude:', str(my_gps.latitude))
-            print('Longitude:', str(my_gps.longitude))
-            # print('Satellites in View:', my_gps.satellites_in_view)
-            # print('Satellites in Use:', my_gps.satellites_in_use)
-            # print('Time Since Last Fix:', my_gps.time_since_fix())
+            return str([my_gps.timestamp, convert_to_decimal(my_gps.latitude), convert_to_decimal(my_gps.longitude)])
+            
         else:
+            sample = [37, 52.51906, 'N']
+            
             print("Waiting for GPS fix...")
-            print("Raw GPS data:", my_sentence)
+            return ""
+            # print("Raw GPS data:", my_sentence)
+            #print(convert_to_decimal(sample))
+            #print_size_in_kb(sample)
+            #print_size_in_kb(convert_to_decimal(sample))
 
         # Optional: Log GPS data
         # my_gps.start_logging('gps_log.txt')
@@ -32,6 +62,11 @@ while True:
 
     else:
         print("No data from GPS module.")
+        return ""
 
-    time.sleep(1)  # Adjust the sleep time as needed
+
+
+#while True:
+#    print(get_packet())
+#    time.sleep(1)  # Adjust the sleep time as needed
 
