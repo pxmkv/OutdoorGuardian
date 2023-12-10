d3454d63716dece7cee0767b0d7f0d955a6c78f1
Katie <katie@pop-os.localdomain>
2023-12-09 16:16:02 -0800

    new file for lora gps callback. next, add distance and dir

---
+++ src/gps_echo.py
@@ -0,0 +1,134 @@
+# Echo any messages received (using custom LoRa parameters).
+#
+# The pin configuration used here is for the first LoRa module of these boards:
+# https://makerfabs.com/esp32-lora-gateway.html
+
+from lora import LoRa
+from machine import Pin, SPI
+from time import sleep
+
+from machine import Pin, I2C
+import ssd1306
+import time
+
+# using default address 0x3C
+i2c = I2C(sda=Pin(21), scl=Pin(22))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+
+
+# SPI pins
+SCK  = 5
+MOSI = 27
+MISO = 19
+# Chip select
+CS   = 18
+# Receive IRQ
+RX   = 26
+
+# Setup SPI
+spi = SPI(
+    1,
+    baudrate=10000000,
+    sck=Pin(SCK, Pin.OUT, Pin.PULL_DOWN),
+    mosi=Pin(MOSI, Pin.OUT, Pin.PULL_UP),
+    miso=Pin(MISO, Pin.IN, Pin.PULL_UP),
+)
+spi.init()
+
+# Setup LoRa
+lora = LoRa(
+    spi,
+    cs=Pin(CS, Pin.OUT),
+    rx=Pin(RX, Pin.IN),
+    frequency=915.0,
+    bandwidth=250000,
+    spreading_factor=10,
+    coding_rate=5,
+)
+
+
+
+def string_to_array(input_str):
+    try:
+        # Remove outer brackets
+        inner_str = input_str.strip('[]')
+
+        # Split the string into main parts
+        parts = inner_str.split('], ')
+
+        # Process first part (nested list)
+        nested_list_str = parts[0].strip('[]')
+        nested_list = []
+        for x in nested_list_str.split(', '):
+            if is_float(x):
+                nested_list.append(float(x) if '.' in x or 'e' in x.lower() or '-' in x else int(x))
+            else:
+                raise ValueError(f"Invalid number format: {x}")
+
+        # Process the remaining parts
+        float_values = []
+        for x in parts[1:]:
+            if is_float(x):
+                float_values.append(float(x))
+            else:
+                raise ValueError(f"Invalid number format: {x}")
+
+        # Combine and return the result
+        return [nested_list] + float_values
+
+    except Exception as e:
+        print(f"Error processing input: {e}")
+        return None
+
+
+# Receive handler
+def handler(x):
+    # Try to parse the received data
+    parsed_data = string_to_array(x)
+    if parsed_data is not None:
+        # If parsing is successful and data is in the correct format
+        print(parsed_data)
+        display.text(str(parsed_data), 0, 0, 1)
+        display.show()
+        display.fill(0)
+        
+        
+
+
+    else:
+        # If the data is not in the correct format
+        print("Invalid data format received")
+
+
+# Set handler
+lora.on_recv(handler)
+# Put module in recv mode
+lora.recv()
+
+def main():
+    # Initialize GPS (if any initialization is needed)
+    gps_data = None
+
+    # Wait for initial GPS fix
+    while gps_data is None:
+        print("Waiting for GPS fix...")
+        gps_data = get_packet()
+        time.sleep(5)  # Check every 5 seconds
+
+    print("GPS fix obtained.")
+
+    # Update location every minute
+    while True:
+        new_data = get_packet()
+        if new_data:
+            gps_data = new_data
+            
+            
+
+        # Wait for one minute before the next update
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
+
